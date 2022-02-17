2a147fdf45613c74af881a1b451dd288e3e94a73
ManukumarNM <manunm.jps@gmail.com>
2022-02-17 21:45:31 +0530

    admin panel checking

--- movieapp/admin.py
+++ movieapp/admin.py
@@ -9,7 +9,7 @@ class MyMovie(admin.ModelAdmin):
 admin.site.register(Movie, MyMovie)
 
 
-class Myprofile(admin.ModelAdmin):
+#class Myprofile(admin.ModelAdmin):
     #list_display = ['user', 'image']
 
 #admin.site.register(Profile, Myprofile)
