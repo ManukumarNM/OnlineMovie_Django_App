580e6fedb9c9cd46bae61c77b8f48edf8baa28ac
ManukumarNM <manunm.jps@gmail.com>
2022-02-21 22:06:19 +0530

    admin panel

--- movieapp/admin.py
+++ movieapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 Register your models here.
 
-#class MyMovie(admin.ModelAdmin):
+class MyMovie(admin.ModelAdmin):
     #list_display = ('name' , 'description', 'year', 'star', 'show')
     #list_filter = ('star', 'show')
 
