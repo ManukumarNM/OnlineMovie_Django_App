9412e448fe003abe74ecc34526c994c977f4b7e9
ManukumarNM <manunm.jps@gmail.com>
2022-02-25 23:15:10 +0530

    forms checking

--- movieapp/forms.py
+++ movieapp/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from django.contrib.auth.models import User 
 from .models import *
 
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['image']
+# class ProfileUpdateForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ['image']
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
